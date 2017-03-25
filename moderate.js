1b7e22ea0bbfe4289e0e4d3974d3c4b51d3cbe82
Maarten Van De Bospoort <maartenb@microsoft.com>
2017-03-24 17:58:16 -0700

    move moderate call to request from unirest

--- moderate.js
+++ moderate.js
@@ -5,6 +5,7 @@ var constants = require('./constants.json');
 var utils = require('./utils.js');
 var uuid = require('node-uuid');
 var auth = require('./auth.js');
+var request = require('request');
 
 module.exports.review = function(contentType, workflow, contentid, input, serverUrl, cb) {
     unirest.post(constants.review_url+constants.moderation_team+'/jobs')
@@ -27,7 +28,7 @@ module.exports.review = function(contentType, workflow, contentid, input, server
         });
 }
 
-module.exports.moderate = function(contentType, input, cb) {
+module.exports.moderate1 = function(contentType, input, cb) {
     switch (contentType){
         case 'ImageUrl':{
             unirest.post(constants.moderation_url+'ProcessImage/Evaluate')
@@ -48,3 +49,30 @@ module.exports.moderate = function(contentType, input, cb) {
             break;
     }
 };
+
+module.exports.moderate = function(contentType, input, cb) {
+    switch (contentType){
+        case 'ImageUrl':{
+            var options = {
+                url: constants.moderation_url+'ProcessImage/Evaluate',
+                headers: {
+                    'content-type':'application/json',
+                    'Ocp-Apim-Subscription-Key': process.env.ocp_key
+                },
+                body: {
+                    'DataRepresentation': 'URL',
+                    'Value': input
+                },
+                json: true,
+                method: 'post'
+            };
+            request(options, function(err, res, body){
+                return cb(err, res.body );
+            })   
+            break;
+        }
+        default:
+            break;
+    }
+};
+
